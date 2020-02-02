3a9c27bbf44427bc84d46369791b5b2782e90362
William Bradley <williambbradley@gmail.com>
2020-02-02 09:52:42 -0700

    ensure test file is not packaged

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/wbbradley/python-parsing",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["test_*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
